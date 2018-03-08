7e2f2afb27bb34b9f22c9f63799cd1b94f849282
KarimAK99 <KarimAbdelKader@outlook.com>
2018-03-08 19:10:40 +0100

    Initial stuff
    basic class and data structures

---
+++ src/backend/Intersection.java
@@ -0,0 +1,13 @@
+package backend;
+
+public class Intersection {
+
+	
+
+	 private SubIntersection left;
+	 private SubIntersection right;
+	 private SubIntersection up;
+	 private SubIntersection down;
+	 
+	 
+}
---
+++ src/backend/TrafficLight.java
@@ -0,0 +1,6 @@
+package backend;
+
+public class TrafficLight {
+
+	private int status;
+}
